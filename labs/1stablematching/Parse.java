ccefea623d533cb8dbea617d75aeec3146133a20
JosefineFrid <josefine.fridh@live.se>
2023-04-19 11:56:10 +0200

    update

--- labs/1stablematching/Parse.java
+++ labs/1stablematching/Parse.java
@@ -19,46 +19,29 @@ public class Parse {
 
         int n = Integer.parseInt(lines.get(0));
         lines.remove(0); //removing the first index once it only tells us n
-        studentPref = new int[n+1][n];
-        companyPref = new int[n+1][n];
-
+        studentPref = new int[n+1][n+1];
+        companyPref = new int[n+1][n+1];
         scanner.close();
     }
 
     public void sortData() {
-
-        //Kör vi detta på första linen dvs den med antalet
-
         for(String line : lines) {
 
             String[] parts = line.split(" ");
             int nbr = Integer.parseInt(parts[0]);
-
-           /* System.out.println("Line: " + line);
-            System.out.println("PartNbr: " + nbr);
-            System.out.println("companyPref[nbr]: " + companyPref[nbr][0] + companyPref[nbr][1]);
-*/
-            if(companyPref[nbr][0] == 0) { //om vi inte har något i detta company nbr
-                //Lägg in preffar i company
+            if(companyPref[nbr][1] == 0) {
                 for(int i=0; i< parts.length; i++) {
-
-                   /* System.out.println("nbr: " + nbr); //ok
-                    System.out.println("i: " + i); //ok
-                    System.out.println("Integer.parseInt(parts[i]): " + Integer.parseInt(parts[i])); //inte ok*/
-                    companyPref[nbr][Integer.parseInt(parts[i])-1] = i;
-
-
-
+                    companyPref[nbr][Integer.parseInt(parts[i])] = i;
                 }
 
             } else { //Annars lägg till delarna i student
-                for(int i=0; i< parts.length; i++) {
-                    studentPref[nbr][Integer.parseInt(parts[i])-1] = i;
+                for (int i = 0; i < parts.length; i++) {
+                    studentPref[nbr][Integer.parseInt(parts[i])] = i;
                 }
-
             }
         }
     }
+
 
     public int[][] getStudentPrefs() {
         return studentPref;
--- labs/1stablematching/Solution.java
+++ labs/1stablematching/Solution.java
@@ -1,52 +1,94 @@
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Solution {
+    static int n;
+    static int[][] companyPrefs;
+    static int[][] studentPrefs;
+    static int[] matches;
+    static LinkedList<Integer> students = new LinkedList<>();
 
     public static void main(String[] args) {
         Parse parser = new Parse();
         parser.sortData();
+        companyPrefs = parser.getcompanyPrefs();
+        studentPrefs = parser.getStudentPrefs();
 
-        int[] matching = match(parser.getStudentPrefs(), parser.getcompanyPrefs());
-        for(int match : matching) {
-            System.out.println(match);
-        }
-
-    }
-
-
-    public static int[] match(int[][] studentPref, int[][] companyPref) {
-        int n = studentPref.length; //Ska den va -1?
-        int[] matches = new int[n];
-        for(int[] student : studentPref) {
-            for(int pref: student) {
-                System.out.print(pref + " ");
+      /*  for (int i = 0; i < studentPrefs.length; i++) {
+            for (int j = 0; j < studentPrefs[i].length; j++) {
+                System.out.print(studentPrefs[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println("n: " + n);
+        System.out.println();
+        for (int i = 0; i < companyPrefs.length; i++) {
+            for (int j = 0; j < companyPrefs[i].length; j++) {
+                System.out.print(companyPrefs[i][j] + " ");
+            }
+            System.out.println();
+        }
+        */
 
+        int n = studentPrefs.length;
+        matches = new int[n];
         Arrays.fill(matches, -1);
 
-        boolean[] studentFree = new boolean[n];
-        Arrays.fill(studentFree, true);
-        studentFree[0] = false;
+        match();
+        for (int i = 1; i< matches.length; i++) {
+            System.out.println(String.valueOf(matches[i]));
+        }
+    }
+    public static void match() {
+        for (int s = 1; s < studentPrefs.length; s++) {
+            students.add(s);
+        }
+        while (!(students.isEmpty())) {
+            int student = students.getFirst();
+            students.removeFirst();
+            int apply = studentPrefs[student][0]+1;
+        //    System.out.println("apply: " + apply);
+            int perferredCompany = 1;
+            for(int i = 1; studentPrefs[student][i] != apply; i++) {
+                perferredCompany++;
+            }
+            studentPrefs[student][0] = studentPrefs[student][0]+1;
+            if (matches[perferredCompany] == -1) {
+                matches[perferredCompany] = student;
+          //      System.out.println("matches :" + Arrays.toString(matches));
+            } else if (companyPrefs[perferredCompany][matches[perferredCompany]] > companyPrefs[perferredCompany][student]) {
+                students.addLast(matches[perferredCompany]);
+                matches[perferredCompany] = student;
+          //      System.out.println("matches :" + Arrays.toString(matches));
+            } else {
+                students.addLast(student);
+            }
+            perferredCompany = 1;
+        }
+    }
+}
 
-        while (true) {
+        /*while (true) {
             int s = -1;
             for (int i = 0; i < n; i++) {
                 if (studentFree[i]) {
-                    s = i;
+                   // System.out.println("StudentFree : " + Arrays.toString(studentFree));
+                    s = i; // Lägger in i i student
                     break;
                 }
             }
             if (s == -1) {
+                // Alla har gåtts igenom?
                 break;
             }
             for (int i = 0; i < n && studentFree[s]; i++) {
                 int c = studentPref[s][i];
+               // System.out.println("C: " + c);
                 if (matches[c] == -1) {
+                  //  System.out.println("ref till comp. : " + c);
+                  //  System.out.println("input i matches 4 company: " + s);
                     matches[c] = s;
                     studentFree[s] = false;
+                  //  System.out.println("matches: " + Arrays.toString(matches));
                 } else {
                     int s1 = matches[c];
                     if (companyPref[c][s] < companyPref[c][s1]) {
@@ -58,7 +100,7 @@ public class Solution {
             }
         }
         return Arrays.copyOfRange(matches, 1, matches.length);
-        //return matches;
-    }
+        //return matches;*/
 
-}
+
+
